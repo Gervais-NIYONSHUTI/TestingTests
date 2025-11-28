c2e8adad989b839e7eea2600a89c9cb266f6bc81
4rGervais <gervaisniyonshuti@gmail.com>
2025-11-28 11:46:08 +0200

    tutorialModal test:

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -2,10 +2,13 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage {
     private WebDriver driver;
     private By seleniumTrainingPage = By.cssSelector(".navbar__links [href='/selenium-training?q=headers']");
+    private By tutorialButton = By.cssSelector(".navbar__tutorial-menu");
+    private By tutorialModal = By.className("overlay");
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
@@ -16,6 +19,10 @@ public class HomePage {
     }
     public void navigationLinks(String selector){
         driver.findElement(By.cssSelector(selector)).click();
+    }
+    public void tutorialModalVisibility(){
+        driver.findElement(tutorialButton).click();
+        ExpectedConditions.visibilityOf(driver.findElement(tutorialModal));
     }
     private void clickLink(By element){
         driver.findElement(seleniumTrainingPage).click();
